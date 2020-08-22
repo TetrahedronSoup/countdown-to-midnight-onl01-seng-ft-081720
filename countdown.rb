9ee05b77dc52f62aaacd3cebf3668b9b2f75be9c
Tristan Catron <trilocat@gmail.com>
2020-08-22 08:35:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,13 @@
 
   countdown = 12
-while countdown <=10
+while countdown <=12
   countdown -= 1
   puts "#{countdown} SECOND(s)!"
 end
 return "HAPPY NEW YEAR!"
-
-def countdown_with_sleep 
+end
+ 
   countdown_with_sleep = 12
+  while countdown_with_sleep <=12
   countdown_with_sleep -= 1
-  
+end
